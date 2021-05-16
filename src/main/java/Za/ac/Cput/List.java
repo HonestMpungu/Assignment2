e36f73ec7aa0d6905ec78938c64bee1c0fb0782e
Honest Mpungu <mpunguhonest@gmail.com>
2021-05-16 19:21:53 +0200

    Assignment2 Committing

--- src/main/java/Za/ac/Cput/List.java
+++ src/main/java/Za/ac/Cput/List.java
@@ -1,4 +1,69 @@
 package Za.ac.Cput;
 
+/**
+ * Honest Mpungu!
+ * Student Number 215072081
+ * Part-Time group
+ * Group AssignmentG
+ */
+
+import java.util.*;
+import java.util.ArrayList;
+
 public class List {
+
+
+    public static void main(String[] args) {
+
+        //List1 can only store String Objects
+        ArrayList<String> list1 = new ArrayList<String>();
+
+        //list2 can store any type of Object
+        ArrayList list2 = new ArrayList();
+
+        //1.adding data to the list
+        list1.add(" Tomatoes");           //01
+        list1.add(" Sausages");          //02
+        list1.add(" Bread");            //03
+        list1.add(" Jam");             //04
+        list1.add("Pork bangers");    //05
+        list1.add("Eggs");          //06
+
+        list2.add("item 1");
+        list2.add("item 2");
+        list2.add("item 3");
+        list2.add("item 4");
+        list2.add("item 5");
+        list2.add("item 6");
+
+        System.out.println("list1 is :" + list1);
+        System.out.println("list2 is :" + list2);
+
+
+        //2. find the element from List
+        String name = list1.get(04);
+        System.out.println("name is :" + name);
+
+        Object o = list2.get(04);
+        System.out.println("o is: " + o);
+
+        //list1.remove("jam");
+        //list2.remove("Item 4");
+        //System.out.println(" List after using removing item 4 namely jam ");
+        // System.out.println("list1 is :"+list1);
+        //System.out.println("list2 is :"+list2);
+
+        ListIterator<String> iter = list1.listIterator();
+        while (iter.hasNext()) {
+            if ("Eggs ".equals(iter.next())) {
+                iter.remove();
+            }
+        }
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+
+    }
 }
+
--- src/main/java/Za/ac/Cput/Map.java
+++ src/main/java/Za/ac/Cput/Map.java
@@ -1,4 +1,43 @@
-package Za.ac.Cput;
+    package Za.ac.Cput;
 
-public class Map {
-}
+/**
+ * Honest Mpungu!
+ * Student Number 215072081
+ * Part-Time group
+ * Group AssignmentG
+ */
+
+    import org.junit.Test;
+
+    import java.util.HashMap;
+    import java.util.Iterator;
+
+    public class Map {
+
+        @Test
+        public void test() {
+
+            HashMap<Integer, String> map = new HashMap<>();
+            map.put(1, "Beans 1");
+            map.put(2, "bacon 2");
+            map.put(3, "Eggs 3");
+            map.put(4, "Mushrooms 4 ");
+            map.put(5, "Pork bangers");
+            map.put(6, "Sausages 6 ");
+            map.put(7, "Bread 7");
+
+            System.out.println(map);
+
+
+            Iterator<Integer> iterator = map.keySet().iterator();
+            if (iterator.hasNext()) {
+                do {
+                    Integer key = iterator.next();
+                    if (key != 6) {
+                        iterator.remove();
+                    }
+                }
+                while (iterator.hasNext());
+            }
+        }
+    }
